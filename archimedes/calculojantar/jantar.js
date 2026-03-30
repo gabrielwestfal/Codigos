6bf59265ba59320506dfc16b7fc6ed22e2278d10
Gabriel <gabrielzrwestfal@gmail.com>
2026-03-30 11:13:00 -0300

    jantar

---
+++ archimedes/calculojantar/jantar.js
@@ -0,0 +1,18 @@
+//criar refereçia para elementos do html
+const inConta = document.getElementById ("inConta")
+const btCalcular = document.getElementById("btCalcular")
+const outTotalconta = document.getElementById ("outTotalconta")
+//Evento do botão e acionar function
+btCalcular.addEventListener("click", btCalcular)
+function btCalcular(){
+
+    var valorConta = Number(inConta.value)
+    var txCarcom = inConta * 0.1
+    var valorTotal = valorConta + txCarcom
+
+    outTotalconta.innerHTML="Taxa do Garçom R$" + txCarcom
+    <br> 
+
+
+}
+
